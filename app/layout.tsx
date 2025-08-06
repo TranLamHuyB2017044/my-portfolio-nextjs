fd822dab0c94035850aef504b703940b4722d446
Huy Tran Lam <tlhuy02@gmail.com>
2025-08-06 23:01:26 +0700

    Update layout and styles, add Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${interFont.className} antialiased`}
+        className={`${interFont.className} bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className=" flex justify-center items-center">Hello word</div>
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
+      <div className="lg:flex lg:justify-between lg:gap-4">
+        <Header/>
+      </div>
+    </div>
   );
 }
