5e7122b8e4e4f754c9845d5ed64dae3ece278f9c
Huy Tran Lam <tlhuy02@gmail.com>
2025-08-07 21:51:47 +0700

    feat: handle hover section nav items

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+'use client'
+
 import GitHubIcon from './icons/GitHubIcon'
+import FacebookIcon from './icons/FaceBookIcon'
+import LinkedInIcon from './icons/LinkedInIcon'
+import { useState } from 'react';
 
 export default function Header() {
+
+    const socialLinks = [
+        {
+            href: "https://github.com/TranLamHuyB2017044",
+            title: "GitHub",
+            icon: <GitHubIcon />,
+        },
+        {
+            href: "https://www.linkedin.com/in/huy-tran-2ba19a253/",
+            title: "LinkedIn",
+            icon: <LinkedInIcon />,
+        },
+        {
+            href: "https://www.facebook.com/tran.lam.huy.180408/",
+            title: "Facebook",
+            icon: <FacebookIcon />,
+        },
+    ];
+
+
+    const navItems = ['About', 'Experience', 'Projects'];
+
+
+
+
     return (
         <header className='flex flex-col justify-between py-24 min-h-screen'>
             <div>
@@ -19,31 +48,43 @@ export default function Header() {
 
                 <section id='navigation'>
                     <ul className='mt-16 text-base-1 w-max uppercase'>
-                        <li className='cursor-pointer flex items-center'>
-                            <div className='h-[1px] mr-4 w-16 bg-base-1'></div>
-                            <p>About</p>
-                        </li>
-                        <li className='cursor-pointer flex items-center'>
-                            <div className='h-[1px] mr-4 w-16 bg-base-1'></div>
-                            <p>Experience</p>
-                        </li>
-                        <li className='cursor-pointer flex items-center'>
-                            <div className='h-[1px] mr-4 w-16 bg-base-1'></div>
-                            <p>About</p>
-                        </li>
-                    </ul>
+                        {navItems.map((item) => (
+                            <li
+                                key={item}
 
+                                className='cursor-pointer flex items-center my-4 group'
+                            >
+                                <div className='h-[1px] mr-4  bg-base-1 group-hover:w-16 w-8 transition-all duration-200'
+                                ></div>
+                                <p
+                                    className={`text-xs duration-200 group-hover:text-base-1 text-base-2 `}
+                                >
+                                    {item}
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
                 </section>
             </div>
 
 
-            <section id='socials'>
-                <div className="flex gap-8">
-                    <a href="">
-                        <GitHubIcon/>
-                    </a>
+            <section id="socials">
+                <div className="flex gap-6">
+                    {socialLinks.map(({ href, title, icon }) => (
+                        <a
+                            key={title}
+                            href={href}
+                            className="hover:text-slate-200"
+                            title={title}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {icon}
+                        </a>
+                    ))}
                 </div>
             </section>
+
 
         </header>
     )
---
+++ app/components/icons/FaceBookIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FacebookIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" fill="currentColor" className="h-6 w-6" aria-hidden="true">
+            <path d="M41 4H9c-2.76 0-5 2.24-5 5v32c0 2.76 2.24 5 5 5h32c2.76 0 5-2.24 5-5V9c0-2.76-2.24-5-5-5zM37 19h-2c-2.14 0-3 .5-3 2v3h5l-1 5h-4v15h-5V29h-4v-5h4v-3c0-4 2-7 6-7 2.9 0 4 1 4 1z" />
+        </svg>
+
+    )
+}
