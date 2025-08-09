80c7c0a89edd430afd5b7b371bd03ce364946510
Huy Tran Lam <tlhuy02@gmail.com>
2025-08-09 16:35:20 +0700

    fix: inactive tab smooth

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ import Experience from "./components/Experience";
 import SectionButton from "./components/SectionButton";
 
 export default function Home() {
-
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [isScrollingByClick, setIsScrollingByClick] = useState(false);
 
   const sectionRefs = [
     useRef<HTMLDivElement>(null),
@@ -20,12 +20,20 @@ export default function Home() {
 
   const handleScrollTo = (index: number) => {
     setActiveIndex(index);
+    setIsScrollingByClick(true);
+
     sectionRefs[index]?.current?.scrollIntoView({ behavior: "smooth" });
+
+    setTimeout(() => {
+      setIsScrollingByClick(false);
+    }, 800);
   };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
+        if (isScrollingByClick) return;
+
         const visibleSection = entries.reduce((max, entry) => {
           return entry.intersectionRatio > max.intersectionRatio ? entry : max;
         }, entries[0]);
@@ -49,26 +57,28 @@ export default function Home() {
     });
 
     return () => observer.disconnect();
-  }, []);
+  }, [isScrollingByClick]);
 
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
       <div className="flex lg:justify-between lg:gap-4 flex-col lg:flex-row">
-        <Header activeIndex={activeIndex} onNavClick={handleScrollTo}/>
+        <Header activeIndex={activeIndex} onNavClick={handleScrollTo} />
         <main id="content" className="lg:w-[52%] mt-24 flex flex-col items-start">
-          <section ref={sectionRefs[0]} id='about' className='mb-16 scroll-mt-16 lg:scroll-mt-24'>
+          <section
+            ref={sectionRefs[0]}
+            id="about"
+            className="mb-16 scroll-mt-16 lg:scroll-mt-24"
+          >
             <SectionButton text="About" />
             <Objective />
           </section>
 
-          <section ref={sectionRefs[1]} id='experience' className='mt-12'>
+          <section ref={sectionRefs[1]} id="experience" className="mt-12">
             <SectionButton text="Experience" />
-
             <Experience />
             <Experience />
             <Experience />
             <Experience />
-
           </section>
 
           <ResumeButton />
@@ -77,7 +87,6 @@ export default function Home() {
             <SectionButton text="Projects" />
             <p>Projects content...</p>
           </section>
-          
         </main>
       </div>
     </div>
