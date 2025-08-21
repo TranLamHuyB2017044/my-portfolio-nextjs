236ed564e169d30b03b5ab9b9243a7a62f26c462
Huy Tran Lam <tlhuy02@gmail.com>
2025-08-21 21:10:33 +0700

    feat: update cv link

--- app/components/ResumeButton.tsx
+++ app/components/ResumeButton.tsx
@@ -4,7 +4,7 @@ import ArrowIcon from './icons/ArrowIcon'
 export default function ResumeButton() {
     return (
         <div className='mb-16 flex items-center justify-center group '>
-            <a href="https://drive.google.com/file/d/1aZhEIn9cupzMDAcImZbAGrtjfqtokrGV/view?usp=sharing" className='text-base-1 group-hover:text-base-3 duration-200 font-semibold' target="_blank"
+            <a href="https://drive.google.com/file/d/1YG-kWLl8Z0RBgpLuN2dqd21-oPu1GVGF/view?usp=sharing" className='text-base-1 group-hover:text-base-3 duration-200 font-semibold' target="_blank"
                 rel="noopener noreferrer">View Full Résumé</a>
             <div className="group-hover:-translate-y-1 group-hover:translate-x-1 duration-200 group-hover:text-base-3">
 
